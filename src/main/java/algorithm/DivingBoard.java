8e11ce43b7a298f24c371a61b032e6e37dc78c49
那位同学 <Hr_Begining@163.com>
2020-07-08 20:47:34 +0800

    2020.07.07 LeetCode刷题--跳水板

---
+++ src/main/java/algorithm/DivingBoard.java
@@ -0,0 +1,26 @@
+package algorithm;
+
+/**
+ * 你正在使用一堆木板建造跳水板。有两种类型的木板，其中长度较短的木板长度为shorter，长度较长的木板长度为longer。
+ * 你必须正好使用k块木板。编写一个方法，生成跳水板所有可能的长度。
+ * 返回的长度需要从小到大排列。
+ *
+ * @author Hr_Ending
+ */
+public class DivingBoard {
+    public int[] divingBoard(int shorter, int longer, int k) {
+        if (k == 0) {
+            return new int[]{};
+        }
+        if (shorter == longer) {
+            return new int[]{shorter * k};
+        }
+        int[] result = new int[k + 1];
+        int start = shorter < longer ? shorter * k : longer * k;
+        int different = shorter < longer ? longer - shorter : shorter - longer;
+        for (int i = 0; i <= k; i++) {
+            result[i] = start + different * i;
+        }
+        return result;
+    }
+}
