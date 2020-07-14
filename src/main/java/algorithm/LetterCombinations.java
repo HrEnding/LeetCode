69e43a8b0bde3fa6dbfc3732de8d6245d32c80f2
那位同学 <Hr_Begining@163.com>
2020-07-14 21:33:27 +0800

    2020.07.13 LeetCode刷题--三角形最小路径、电话按键字母组合

---
+++ src/main/java/algorithm/LetterCombinations.java
@@ -0,0 +1,64 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+ * <p>
+ * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母
+ *
+ * @author 21489
+ */
+public class LetterCombinations {
+    private static final String[] LETTER_MAP = {
+            //0
+            " ",
+            //1
+            "",
+            //2
+            "abc",
+            //3
+            "def",
+            //4
+            "ghi",
+            //5
+            "jkl",
+            //6
+            "mno",
+            //7
+            "pqrs",
+            //8
+            "tuv",
+            //9
+            "wxyz"
+    };
+
+    private ArrayList<String> res;
+
+    public List<String> letterCombinations(String digits) {
+
+        res = new ArrayList<String>();
+        if (digits.equals("")) {
+            return res;
+        }
+
+        findCombination(digits, 0, "");
+        return res;
+    }
+
+    private void findCombination(String digits, int index, String s) {
+
+        if (index == digits.length()) {
+            res.add(s);
+            return;
+        }
+
+        Character c = digits.charAt(index);
+        String letters = LETTER_MAP[c - '0'];
+        for (int i = 0; i < letters.length(); i++) {
+            findCombination(digits, index + 1, s + letters.charAt(i));
+        }
+    }
+
+}
---
+++ src/main/java/algorithm/MinimumTotal.java
@@ -0,0 +1,29 @@
+package algorithm;
+
+import java.util.List;
+
+/**
+ * 给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。
+ * <p>
+ * 相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。
+ *
+ * @author 21489
+ */
+public class MinimumTotal {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if (null == triangle || triangle.isEmpty()) {
+            return 0;
+        }
+        // 只需要记录每一层的最小值即可
+        int[] dp = new int[triangle.size() + 1];
+
+        for (int i = triangle.size() - 1; i >= 0; i--) {
+            List<Integer> curTr = triangle.get(i);
+            for (int j = 0; j < curTr.size(); j++) {
+                //这里的dp[j] 使用的时候默认是上一层的，赋值之后变成当前层
+                dp[j] = Math.min(dp[j], dp[j + 1]) + curTr.get(j);
+            }
+        }
+        return dp[0];
+    }
+}
