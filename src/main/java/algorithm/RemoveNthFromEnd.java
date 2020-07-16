594baa6c593a0d45993568b9ef7c798d0212aefb
那位同学 <Hr_Begining@163.com>
2020-07-16 21:19:56 +0800

    2020.07.16 LeetCode刷题--判断二分图、删除链表倒数第N个元素

---
+++ src/main/java/algorithm/RemoveNthFromEnd.java
@@ -0,0 +1,34 @@
+package algorithm;
+
+/**
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ *
+ * @Author: Hr_Ending
+ * @Date: 2020/7/16 21:14
+ */
+public class RemoveNthFromEnd {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+        ListNode start = pre, end = pre;
+        while (n != 0) {
+            start = start.next;
+            n--;
+        }
+        while (start.next != null) {
+            start = start.next;
+            end = end.next;
+        }
+        end.next = end.next.next;
+        return pre.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
