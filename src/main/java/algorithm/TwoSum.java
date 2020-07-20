c00946ef0b6e0b0d250c10da01ad8aae0e27404e
那位同学 <Hr_Begining@163.com>
2020-07-20 20:54:57 +0800

    2020.07.16 LeetCode刷题

---
+++ src/main/java/algorithm/TwoSum.java
@@ -0,0 +1,31 @@
+package algorithm;
+
+/**
+ * 给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+ * <p>
+ * 函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+ * <p>
+ * 说明:
+ * <p>
+ * 返回的下标值（index1 和 index2）不是从零开始的。
+ * 你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+ *
+ * @Author: Hr_Ending
+ * @Date: 2020/7/20 20:23
+ */
+public class TwoSum {
+    public int[] twoSum(int[] numbers, int target) {
+        int low = 0, high = numbers.length - 1;
+        while (low < high) {
+            int sum = numbers[low] + numbers[high];
+            if (sum == target) {
+                return new int[]{low + 1, high + 1};
+            } else if (sum < target) {
+                ++low;
+            } else {
+                --high;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
