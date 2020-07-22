f89183bdb0e44d827385f34b44410a42498cd00b
那位同学 <Hr_Begining@163.com>
2020-07-22 21:31:34 +0800

    2020.07.22 LeetCode刷题

--- src/main/java/algorithm/LongestPalindrome.java
+++ src/main/java/algorithm/LongestPalindrome.java
@@ -8,24 +8,41 @@ package algorithm;
  */
 public class LongestPalindrome {
     public String longestPalindrome(String s) {
-        if (s.length() < 2 || s.substring(0, 1).equals(s.substring(s.length() - 1))) {
+        // 特判
+        int len = s.length();
+        if (len < 2) {
             return s;
         }
-        int maxLen = 0;
-        String[] strings = new String[s.length()];
-        char[] charArr = s.toCharArray();
-        for (int i = 0; i < s.length(); i++) {
-            StringBuilder str = new StringBuilder().append(charArr[i]);
-            int length = 1;
-            for (int j = i + 1; j < s.length(); j++) {
-                str.append(charArr[j]);
-                length++;
-                if (charArr[i] == charArr[j] && length > maxLen) {
-                    maxLen = length;
-                    strings[length] = str.toString();
+
+        int maxLen = 1;
+        int begin = 0;
+
+        // dp[i][j] 表示 s[i, j] 是否是回文串
+        boolean[][] dp = new boolean[len][len];
+        char[] charArray = s.toCharArray();
+
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+        for (int j = 1; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+
+                // 只要 dp[i][j] == true 成立，就表示子串 s[i..j] 是回文，此时记录回文长度和起始位置
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
                 }
             }
         }
-        return maxLen == 0 ? s.substring(0, 1) : strings[maxLen];
+        return s.substring(begin, begin + maxLen);
     }
 }
