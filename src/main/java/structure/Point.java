03e037c72f3e112c1d848538b550a63a0744f6c1
那位同学 <Hr_Begining@163.com>
2020-07-29 21:50:38 +0800

    2020.07.27 LeetCode刷题

---
+++ src/main/java/structure/Point.java
@@ -0,0 +1,19 @@
+package structure;
+
+/**
+ * 节点结构
+ *
+ * @Author: Hr_Ending
+ * @Date: 2020/7/29 21:46
+ */
+public class Point {
+    public int x, y;
+
+    public Point() {
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
