c7b1dfda04a967cd87e629b5e76959222218d7e8
那位同学 <Hr_Begining@163.com>
2020-07-31 22:08:06 +0800

    2020.07.31 LeetCode刷题

---
+++ src/main/java/algorithm/FindMagicIndex.java
@@ -0,0 +1,27 @@
+package algorithm;
+
+/**
+ * 魔术索引。 在数组A[0...n-1]中，有所谓的魔术索引，满足条件A[i] = i。给定一个有序整数数组，编写一种方法找出魔术索引，若有的话，在数组A中找出一个魔术索引，如果没有，则返回-1。若有多个魔术索引，返回索引值最小的一个。
+ *
+ * @Author: Hr_Ending
+ * @Date: 2020/7/31 22:04
+ */
+public class FindMagicIndex {
+    public int findMagicIndex(int[] nums) {
+        return getAnswer(nums, 0, nums.length - 1);
+    }
+
+    public int getAnswer(int[] nums, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+        int mid = (right - left) / 2 + left;
+        int leftAnswer = getAnswer(nums, left, mid - 1);
+        if (leftAnswer != -1) {
+            return leftAnswer;
+        } else if (nums[mid] == mid) {
+            return mid;
+        }
+        return getAnswer(nums, mid + 1, right);
+    }
+}
---
+++ src/main/java/algorithm/IsMatch.java
@@ -0,0 +1,46 @@
+package algorithm;
+
+/**
+ * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+ * <p>
+ * '.' 匹配任意单个字符
+ * '*' 匹配零个或多个前面的那一个元素
+ * 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
+ * <p>
+ * 说明:
+ * <p>
+ * s 可能为空，且只包含从 a-z 的小写字母。
+ * p 可能为空，且只包含从 a-z 的小写字母，以及字符 . 和 *。
+ *
+ * @Author: Hr_Ending
+ * @Date: 2020/7/31 22:06
+ */
+public class IsMatch {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 0; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) != '*') {
+                    dp[i][j] = match(s, p, i, j) && dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = match(s, p, i, j - 1) ? (dp[i][j - 2] || dp[i - 1][j]) : dp[i][j - 2];
+                }
+            }
+        }
+        return dp[m][n];
+
+    }
+
+    public boolean match(String s, String p, int i, int j) {
+        if (i == 0) {
+            return false;
+        }
+        if (p.charAt(j - 1) == '.') {
+            return true;
+        }
+        return s.charAt(i - 1) == p.charAt(j - 1);
+    }
+}
