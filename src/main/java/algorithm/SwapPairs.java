f850c7d2b2f3e6e1caa2eba94823c5cd12cb2627
那位同学 <Hr_Begining@163.com>
2020-08-03 20:54:15 +0800

    2020.08.03 LeetCode刷题

---
+++ src/main/java/algorithm/SwapPairs.java
@@ -0,0 +1,23 @@
+package algorithm;
+
+import structure.ListNode;
+
+/**
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ * <p>
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ *
+ * @Author: Hr_Ending
+ * @Date: 2020/8/3 20:53
+ */
+public class SwapPairs {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        return next;
+    }
+}
