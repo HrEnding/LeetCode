c349c5fa3191ff125911f930759f8ffbf1b03d9a
那位同学 <Hr_Begining@163.com>
2020-08-21 21:00:06 +0800

    2020.08.21 LeetCode刷题

---
+++ src/main/java/algorithm/MinDepth.java
@@ -0,0 +1,26 @@
+package algorithm;
+
+import structure.TreeNode;
+
+/**
+ * 给定一个二叉树，找出其最小深度。
+ * <p>
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ * <p>
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * @Author: Hr_Ending
+ * @Date: 2020/8/21 20:55
+ */
+public class MinDepth {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int m1 = minDepth(root.left);
+        int m2 = minDepth(root.right);
+        //1.如果左孩子和右孩子有为空的情况，直接返回m1+m2+1
+        //2.如果都不为空，返回较小深度+1
+        return root.left == null || root.right == null ? m1 + m2 + 1 : Math.min(m1, m2) + 1;
+    }
+}
---
+++ src/main/java/algorithm/Rotate.java
@@ -0,0 +1,36 @@
+package algorithm;
+
+/**
+ * 给定一个 n × n 的二维矩阵表示一个图像。
+ * <p>
+ * 将图像顺时针旋转 90 度。
+ * <p>
+ * 说明：
+ * <p>
+ * 你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+ *
+ * @Author: Hr_Ending
+ * @Date: 2020/8/21 20:56
+ */
+public class Rotate {
+    public void rotate(int[][] matrix) {
+        if (matrix.length == 0 || matrix.length != matrix[0].length) {
+            return;
+        }
+        int nums = matrix.length;
+        for (int i = 0; i < nums; ++i) {
+            for (int j = 0; j < nums - i; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[nums - 1 - j][nums - 1 - i];
+                matrix[nums - 1 - j][nums - 1 - i] = temp;
+            }
+        }
+        for (int i = 0; i < (nums >> 1); ++i) {
+            for (int j = 0; j < nums; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[nums - 1 - i][j];
+                matrix[nums - 1 - i][j] = temp;
+            }
+        }
+    }
+}
