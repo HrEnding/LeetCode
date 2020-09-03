5b38c5dad7e63c0884e41005afb4290f796e00f7
那位同学 <Hr_Begining@163.com>
2020-09-03 21:22:01 +0800

    2020.09.03 LeetCode刷题

---
+++ src/main/java/algorithm/SolveNQueens.java
@@ -0,0 +1,88 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ * 给定一个整数 n，返回所有不同的 n 皇后问题的解决方案。
+ * <p>
+ * 每一种解法包含一个明确的 n 皇后问题的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+ *
+ * @Author: Hr_Ending
+ * @Date: 2020/9/3 21:15
+ */
+public class SolveNQueens {
+    public List<List<String>> solveNQueens(int n) {
+        char[][] chess = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                chess[i][j] = '.';
+            }
+        }
+        List<List<String>> res = new ArrayList<>();
+        solve(res, chess, 0);
+        return res;
+    }
+
+    private void solve(List<List<String>> res, char[][] chess, int row) {
+        if (row == chess.length) {
+            res.add(construct(chess));
+            return;
+        }
+        for (int col = 0; col < chess.length; col++) {
+            if (valid(chess, row, col)) {
+                chess[row][col] = 'Q';
+                solve(res, chess, row + 1);
+                chess[row][col] = '.';
+            }
+        }
+    }
+
+    /**
+     * 判断行列斜线重复皇后
+     *
+     * @param chess
+     * @param row
+     * @param col
+     * @return
+     */
+    private boolean valid(char[][] chess, int row, int col) {
+        //判断当前列有没有皇后,因为他是一行一行往下走的，
+        //我们只需要检查走过的行数即可，通俗一点就是判断当前
+        //坐标位置的上面有没有皇后
+        for (int i = 0; i < row; i++) {
+            if (chess[i][col] == 'Q') {
+                return false;
+            }
+        }
+        //判断当前坐标的右上角有没有皇后
+        for (int i = row - 1, j = col + 1; i >= 0 && j < chess.length; i--, j++) {
+            if (chess[i][j] == 'Q') {
+                return false;
+            }
+        }
+        //判断当前坐标的左上角有没有皇后
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (chess[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 把数组转换成List
+     *
+     * @param chess
+     * @return
+     */
+    private List<String> construct(char[][] chess) {
+        List<String> path = new ArrayList<>();
+        for (int i = 0; i < chess.length; i++) {
+            path.add(new String(chess[i]));
+        }
+        return path;
+    }
+
+}
