da54e63e22d6def21ae5535ec62296d1ad6df642
那位同学 <Hr_Begining@163.com>
2020-09-15 21:28:33 +0800

    2020.09.15 LeetCode刷题

---
+++ src/main/java/algorithm/MergeKLists.java
@@ -0,0 +1,55 @@
+package algorithm;
+
+import structure.ListNode;
+
+/**
+ * 给你一个链表数组，每个链表都已经按升序排列。
+ * <p>
+ * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+ * <p>
+ * 提示：
+ * <p>
+ * k == lists.length
+ * 0 <= k <= 10^4
+ * 0 <= lists[i].length <= 500
+ * -10^4 <= lists[i][j] <= 10^4
+ * lists[i] 按 升序 排列
+ * lists[i].length 的总和不超过 10^4
+ *
+ * @Author: Hr_Ending
+ * @Date: 2020/9/15 21:26
+ */
+public class MergeKLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return helper(lists, 0, lists.length - 1);
+    }
+
+    //通过mid将数组一分为二，并不断缩小规模，当规模为1时返回并开始合并
+    //通过合并两个链表，不断增大其规模，整体看就是不断缩小-最后不断扩大的过程
+    private ListNode helper(ListNode[] lists, int begin, int end) {
+        if (begin == end) {
+            return lists[begin];
+        }
+        int mid = begin + (end - begin) / 2;
+        ListNode left = helper(lists, begin, mid);
+        ListNode right = helper(lists, mid + 1, end);
+        return merge(left, right);
+    }
+
+    //合并两个有序链表
+    private ListNode merge(ListNode a, ListNode b) {
+        if (a == null || b == null) {
+            return (a == null) ? b : a;
+        }
+        if (a.val <= b.val) {
+            a.next = merge(a.next, b);
+            return a;
+        } else {
+            b.next = merge(a, b.next);
+            return b;
+        }
+    }
+}
