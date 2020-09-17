b6a9055986abf97ec357ae9948939878cf421ac9
那位同学 <Hr_Begining@163.com>
2020-09-17 23:21:48 +0800

    2020.09.15 LeetCode刷题

---
+++ src/main/java/algorithm/FindRedundantDirectedConnection.java
@@ -0,0 +1,52 @@
+package algorithm;
+
+import structure.UnionFind;
+
+/**
+ * 在本问题中，有根树指满足以下条件的有向图。该树只有一个根节点，所有其他节点都是该根节点的后继。每一个节点只有一个父节点，除了根节点没有父节点。
+ * <p>
+ * 输入一个有向图，该图由一个有着N个节点 (节点值不重复1, 2, ..., N) 的树及一条附加的边构成。附加的边的两个顶点包含在1到N中间，这条附加的边不属于树中已存在的边。
+ * <p>
+ * 结果图是一个以边组成的二维数组。 每一个边 的元素是一对 [u, v]，用以表示有向图中连接顶点 u 和顶点 v 的边，其中 u 是 v 的一个父节点。
+ * <p>
+ * 返回一条能删除的边，使得剩下的图是有N个节点的有根树。若有多个答案，返回最后出现在给定二维数组的答案。
+ *
+ * @Author: Hr_Ending
+ * @Date: 2020/9/17 23:18
+ */
+public class FindRedundantDirectedConnection {
+    public int[] findRedundantDirectedConnection(int[][] edges) {
+        int nodesCount = edges.length;
+        UnionFind uf = new UnionFind(nodesCount + 1);
+        int[] parent = new int[nodesCount + 1];
+        for (int i = 1; i <= nodesCount; ++i) {
+            parent[i] = i;
+        }
+        int conflict = -1;
+        int cycle = -1;
+        for (int i = 0; i < nodesCount; ++i) {
+            int[] edge = edges[i];
+            int node1 = edge[0], node2 = edge[1];
+            if (parent[node2] != node2) {
+                conflict = i;
+            } else {
+                parent[node2] = node1;
+                if (uf.find(node1) == uf.find(node2)) {
+                    cycle = i;
+                } else {
+                    uf.union(node1, node2);
+                }
+            }
+        }
+        if (conflict < 0) {
+            return new int[]{edges[cycle][0], edges[cycle][1]};
+        } else {
+            int[] conflictEdge = edges[conflict];
+            if (cycle >= 0) {
+                return new int[]{parent[conflictEdge[1]], conflictEdge[1]};
+            } else {
+                return new int[]{conflictEdge[0], conflictEdge[1]};
+            }
+        }
+    }
+}
---
+++ src/main/java/structure/UnionFind.java
@@ -0,0 +1,27 @@
+package structure;
+
+/**
+ * @Author: Hr_Ending
+ * @Date: 2020/9/17 23:19
+ */
+public class UnionFind {
+    public int[] ancestor;
+
+    public UnionFind(int n) {
+        ancestor = new int[n];
+        for (int i = 0; i < n; ++i) {
+            ancestor[i] = i;
+        }
+    }
+
+    public void union(int index1, int index2) {
+        ancestor[find(index1)] = find(index2);
+    }
+
+    public int find(int index) {
+        if (ancestor[index] != index) {
+            ancestor[index] = find(ancestor[index]);
+        }
+        return ancestor[index];
+    }
+}
