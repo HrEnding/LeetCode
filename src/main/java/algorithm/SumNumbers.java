f3307fe3af96dc269729a00fa78cdea29d53f511
那位同学 <Hr_Begining@163.com>
2020-10-29 20:55:16 +0800

    2020.10.29 LeetCode刷题

---
+++ src/main/java/algorithm/SumNumbers.java
@@ -0,0 +1,33 @@
+package algorithm;
+
+import structure.TreeNode;
+
+/**
+ * 给定一个二叉树，它的每个结点都存放一个 0-9 的数字，每条从根到叶子节点的路径都代表一个数字。
+ * <p>
+ * 例如，从根到叶子节点路径 1->2->3 代表数字 123。
+ * <p>
+ * 计算从根到叶子节点生成的所有数字之和。
+ * <p>
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * @Author: Hr_Ending
+ * @Date: 2020/10/29 20:53
+ */
+public class SumNumbers {
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    public int dfs(TreeNode root, int prevSum) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = prevSum * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            return sum;
+        } else {
+            return dfs(root.left, sum) + dfs(root.right, sum);
+        }
+    }
+}
